19f8552d75f3706ac87e5a455b9464dc0925de28
meijunjie <meijunjie@le.com>
2018-11-02 17:54:01 +0800

    initalize reposity

---
+++ src/main/java/chapter1/BinarySearch.java
@@ -0,0 +1,13 @@
+package chapter1;
+
+/**
+ * 2分查找
+ * @author meijunjie
+ * @date 2018/11/2
+ */
+public class BinarySearch {
+
+    public static void main(String[] args){
+        // something to do
+    }
+}
